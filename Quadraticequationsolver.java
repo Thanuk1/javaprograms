3ad41cff4486540822ca91935355ee5f3ab93012
Thanuk1 <127390112+Thanuk1@users.noreply.github.com>
2023-03-09 14:57:18 +0530

    quadratic

---
+++ Quadraticequationsolver.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class QuadraticEquationSolver {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the coefficient of x^2: ");
+        double a = input.nextDouble();
+        System.out.print("Enter the coefficient of x: ");
+        double b = input.nextDouble();
+        System.out.print("Enter the constant: ");
+        double c = input.nextDouble();
+        
+        double discriminant = b * b - 4 * a * c;
+        if (discriminant < 0) {
+            System.out.println("The quadratic equation has no real roots.");
+        } else if (discriminant == 0) {
+            double root = -b / (2 * a);
+            System.out.println("The quadratic equation has one root: " + root);
+        } else {
+            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            System.out.println("The quadratic equation has two roots: " + root1 + " and " + root2);
+        }
+    }
+}
